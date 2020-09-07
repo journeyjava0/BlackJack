6719945e17aece45baa39592ece172ec53a264a7
unknown <Journeyjava0@gmail.com>
2020-09-06 20:01:20 -0600

    final update

--- Player.java
+++ Player.java
@@ -6,7 +6,10 @@ public class Player {
     private String name;
     private Hand hand;
     private int playerBet;
-    private int pot;
+    private static int pot = 0;
+    private static int numWins = 0;
+    private static int computerWins = 0;
+    private static int gamesPlayed = 0;
 
     public Player(String name) {
         this.name = name;
@@ -15,27 +18,25 @@ public class Player {
 
     //gets the user's bet
     public void getBet() {
-        Scanner bet = new Scanner(System.in);
-        int count = 0;
-        do {
-            System.out.println("How much would you like to start with? $");
-            System.out.println("Each bet is automatically 10% of your remaining pot");
-            while (!bet.hasNextInt()) {
-                System.out.println("That's not a valid input " + name + " let's try that again!");
-                bet.next();
-                count++;
-            }
-            pot = bet.nextInt();
-        } while (pot <= 0);
 
-        if (count > 0) {
-            System.out.println("That's better " + name);
+        Scanner bet = new Scanner(System.in);
+
+        if (gamesPlayed == 0) {
+            System.out.println("Each bet is automatically 10% of your initial pot");
+            System.out.println("How much would you like to start with? $");
+
+        //See if they provided a valid input
+            validInput(bet);
+
+            pot = bet.nextInt();
+            System.out.println("Really? Are you sure you want to bet $" + pot + "? Well, too late now, good luck!");
+
+            if (gamesPlayed > 0) {
+                System.out.println("You have $" + getPot() + " left.");
+            }
         }
-        System.out.println("Really? Are you sure you want to bet $" + pot + "? Well, too late now, good luck!");
-        playerBet = (int) Math.ceil(pot * .1);
     }
 
-    //Returns the total pot ... how to calculate each time they bet how much they have left?
     public int getPot() {
         return pot;
     }
@@ -45,6 +46,7 @@ public class Player {
     }
 
     public int getPlayerBet() {
+        playerBet = (int) Math.ceil(pot * .1);
         return playerBet;
     }
 
@@ -56,6 +58,24 @@ public class Player {
         return name;
     }
 
+    public boolean validInput(Scanner bet){
+
+        int count = 0;
+
+        if(!bet.hasNextInt()) {
+            do {
+                System.out.println("That's not a valid input " + name + " let's try that again!");
+                bet.next();
+                if(bet.hasNextInt()){
+                    continue;
+                }
+                count++;
+            } while (!bet.hasNextInt());
+        }
+        if (count > 0) {
+            System.out.println("That's better " + name);
+        } return bet.hasNextInt();
+    } 
 
     public String getName() {
         return name;
@@ -73,11 +93,43 @@ public class Player {
         this.hand = hand;
     }
 
+    //Keep track of user wins separately from computer
+    public int getNumWinsP1() {
+        return numWins;
+    }
+
+    //Need to keep track of computer wins separately from user wins
+    public int getNumWinsP2(){
+        return computerWins;
+    }
+
+    //Keep track of user wins separately from computer
+    public void setNumWinsP1(int numWins) {
+        this.numWins = numWins;
+    }
+
+    //Need to keep track of computer wins separately from user wins
+    public void setNumWinsP2(int computerWins){
+        this.computerWins = computerWins;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
     @Override
     public String toString() {
         return "Player{" +
                 "name='" + name + '\'' +
                 ", hand=" + hand +
                 '}';
+    }
+
+    public void clearHand() {
+        hand = new Hand();
     }
 }
